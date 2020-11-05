1d1b204f1108139ad2863f83567284664bf4c889
Michael Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2020-11-05 19:00:44 +0300

    Add files via upload

---
+++ SearchDB/APM/Manager.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class Manager {
+public:
+  void get_package(){
+
+      //code here
+  }
+
+};
---
+++ SearchDB/APM/main.cpp
@@ -0,0 +1,14 @@
+//Archive package manager
+#include <iostream>
+#include <string>
+#include "Manager.cpp";
+
+using namespace std;
+
+int main()
+{
+
+    Manager obj;
+    obj.get_package();
+    return 0;
+}
